932c37c517f61b7655deb9030b6c17c7003faba8
Administrator <gaoyongqiang@xrateverse.com>
2026-01-27 15:15:33 +0800

    refactor: 移除冗余映射器位置并调整 MyBatis 配置

--- mall-admin/src/main/java/com/macro/mall/config/MyBatisConfig.java
+++ mall-admin/src/main/java/com/macro/mall/config/MyBatisConfig.java
@@ -1,6 +1,5 @@
 package com.macro.mall.config;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -10,6 +9,5 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @Configuration
 @EnableTransactionManagement
-@MapperScan({"com.macro.mall.mapper","com.macro.mall.dao"})
 public class MyBatisConfig {
 }
