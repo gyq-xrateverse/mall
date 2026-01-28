f12148c864fd5c116867fee35c796edccfc29cc4
Administrator <gaoyongqiang@xrateverse.com>
2026-01-28 17:14:26 +0800

    添加新的docker配置

--- .bash/docker-login.sh
+++ .bash/docker-login.sh
@@ -8,3 +8,14 @@ ALIYUN_DOCKER_TOKEN
 beilv_docker_123
 
 docker login crpi-4z4v1n5g8hbg9g3x.cn-hangzhou.personal.cr.aliyuncs.com
+
+ALIYUN_DOCKER_REGISTRY
+crpi-ni3ahd37mttrjwgx.cn-hangzhou.personal.cr.aliyuncs.com
+
+ALIYUN_DOCKER_USER
+oceanwoo
+
+ALIYUN_DOCKER_TOKEN
+RAteV0908#@
+
+docker login crpi-ni3ahd37mttrjwgx.cn-hangzhou.personal.cr.aliyuncs.com
